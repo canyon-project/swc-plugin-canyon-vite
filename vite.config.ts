6db11ce9032dfd9849f8a6481971a907163f0afe
zhangtao25 <wr.zhang25@gmail.com>
2024-11-27 11:27:07 +0800

    upd

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react({
+    plugins:[
+        ['swc-plugin-coverage-instrument',{}],
+        ['swc-plugin-canyon',{}]
+    ]
+  })],
+})
